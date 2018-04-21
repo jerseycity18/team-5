9ab501a444a381285f4eaf910c7a87cc20dd4a63
Koushik Thai <thaikoushik@gmail.com>
2018-04-21 16:19:56 -0400

    Add files via upload

--- BestBuddiesMatching/src/com/bestbuddies/test/AddBuddies.java
+++ BestBuddiesMatching/src/com/bestbuddies/test/AddBuddies.java
@@ -20,13 +20,20 @@ public class AddBuddies {
 		b5.setZipcode(Long.valueOf("07306"));
 		b6.setZipcode(Long.valueOf("07123"));*/
 		b1.setDistance(10);
-		b1.setDistance(20);
-		b1.setDistance(30);
-		b1.setDistance(40);
-		b1.setDistance(50);
-		b1.setDistance(60);
-		List<Buddy> listOfBuddies = new ArrayList<Buddy>();
-		List<Buddy> listOfVounteers = new ArrayList<Buddy>();
+		b2.setDistance(20);
+		b3.setDistance(30);
+		b4.setDistance(40);
+		b5.setDistance(50);
+		b6.setDistance(60);
+		b1.setInterests(new String[]{"Adventure","Baking","Basketball","Bowling","Comedy","Cooking","Honesty","Manicures","Movies","Park","Restaurants","Tennis","Theater","Yoga","Museums"});
+		b2.setInterests(new String[]{"Adventure","Bowling","Comedy","Cooking","Honesty","Manicures","Movies","Park","Restaurants","Tennis","Theater","Yoga","Museums"});
+		b3.setInterests(new String[]{"Adventure","Baking","Comedy","Cooking","Honesty","Manicures","Movies","Park","Restaurants","Tennis","Theater","Yoga","Museums"});
+		b4.setInterests(new String[]{"Adventure","Baking","Basketball","Bowling","Honesty","Manicures","Movies","Park","Restaurants","Tennis","Theater","Yoga","Museums"});
+		b5.setInterests(new String[]{"Adventure","Bowling","Comedy","Cooking","Honesty","Manicures","Movies","Park","Restaurants","Tennis","Theater","Yoga","Museums"});
+		b6.setInterests(new String[]{"Adventure","Baking","Basketball","Bowling","Comedy","Manicures","Movies","Park","Restaurants"});
+		
+		ArrayList<Buddy> listOfBuddies = new ArrayList<Buddy>();
+		ArrayList<Buddy> listOfVounteers = new ArrayList<Buddy>();
 		listOfBuddies.add(b1);
 		listOfBuddies.add(b2);
 		listOfBuddies.add(b3);
